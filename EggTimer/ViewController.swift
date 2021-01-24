f735291e001bf420fda01146dabf529100eb2377
Thin Myat Noe <thinmyatnoe@Thins-MacBook-Pro.local>
2021-01-24 23:08:31 +1100

    bug fixed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,35 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var labelHandler: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
     let eggTimes = ["Soft" : 3, "Medium": 4, "Hard" : 7]      // convert minutes to seconds
-    var totalTime = 0
-    var secondsPassed = 0
+    var totalTime: Float = 0
+    var secondsPassed: Float = 0
     var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        progressBar.progress = 1.0
         timer.invalidate()
         let hardness = sender.currentTitle!
-        totalTime = eggTimes[hardness]!
+        labelHandler.text = hardness
+        totalTime = Float(eggTimes[hardness]!)
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0.0
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
         if secondsPassed < totalTime {
-            var percentageProgress = secondsPassed/totalTime
-            progressBar.progress = Float(percentageProgress)
+            progressBar.progress = secondsPassed/totalTime
             secondsPassed += 1
         }
         else {
+            playSound()
             timer.invalidate()
+            progressBar.progress = 1.0
             labelHandler.text = "DONE!"
         }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
